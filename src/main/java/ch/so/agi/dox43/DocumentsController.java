8e2a7e3096fcb20e509c3a07533df1ffe07476c2
Stefan Ziegler <edi.gonzales@gmail.com>
2024-03-27 14:43:36 +0100

    wip

---
+++ src/main/java/ch/so/agi/dox43/DocumentsController.java
@@ -0,0 +1,26 @@
+package ch.so.agi.dox43;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DocumentsController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    //https://geo.so.ch/api/v1/document/kantonsgrenzsteine?feature=22239&x=2620446.1060485328&y=1257669.3676100764&crs=EPSG%3A2056
+    @GetMapping(path = "/reports/{format}")
+    public ResponseEntity<?> getDocument(@PathVariable("format") String format,
+            @RequestParam(name = "DocTemplate", required = true) String docTemplate,
+            @RequestParam Map<String, String> queryParameters) throws Exception {
+
+        
+        return null;
+    }
+}
