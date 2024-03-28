f53f47c63f09c028673a95fbe0583a3e55bb89be
Stefan Ziegler <edi.gonzales@gmail.com>
2024-03-28 10:30:41 +0100

    start with pure xslfo approach

--- src/main/java/ch/so/agi/dox43/DocumentsController.java
+++ src/main/java/ch/so/agi/dox43/DocumentsController.java
@@ -17,9 +17,8 @@ public class DocumentsController {
     // https://geo.so.ch/api/v1/document/kantonsgrenzsteine?feature=22239&x=2620446.1060485328&y=1257669.3676100764&crs=EPSG%3A2056
     // https://geo.so.ch/api/v1/document/grundstuecksbeschrieb?feature=21396731&x=2607902.4078048863&y=1228274.0228574278&crs=EPSG%3A2056
     // https://geo.so.ch/api/v1/document/grundstuecksbeschrieb?feature=21361869&x=2608068.8582796236&y=1228108.4535374695&crs=EPSG%3A2056
-    @GetMapping(path = "/reports/{format}")
-    public ResponseEntity<?> getDocument(@PathVariable("format") String format,
-            @RequestParam(name = "DocTemplate", required = true) String docTemplate,
+    @GetMapping(path = "/documents/{document}")
+    public ResponseEntity<?> getDocument(@PathVariable("document") String document,
             @RequestParam Map<String, String> queryParameters) throws Exception {
 
         
